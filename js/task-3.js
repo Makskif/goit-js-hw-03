ce6f7796a8e1c1035923b8ed6e453a2891949a12
Максим Ляшко <makskif1@gmail.com>
2024-06-12 13:23:58 +0300

    dz3

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,17 @@
 function filterArray(numbers, value) {
-   
-  const number = numbers.length ;
+  const number = numbers.length;
   let count = 0;
+  let result = [];
 
-let result = ([]);
   while (count < number) {
-
     if (numbers[count] > value) {
-       result = result.concat(numbers[count]);
+      result = result.concat(numbers[count]);
     } else {
     }
-   
+
     count += 1;
   }
-  
+
   return result;
 }
 
@@ -22,3 +20,4 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76, 26, 42, 56, 41, 76, 26, 42, 56], 20));
